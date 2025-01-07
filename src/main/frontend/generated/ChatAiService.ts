299332e9efd9a1bc748e9d9e1c23bf53de2b0d53
Sami-Khairy <samikhairy2003@gmail.com>
2025-01-07 12:50:31 +0100

    add rag api using openai & local file storage

--- src/main/frontend/generated/ChatAiService.ts
+++ src/main/frontend/generated/ChatAiService.ts
@@ -1,4 +1,5 @@
 import { EndpointRequestInit as EndpointRequestInit_1 } from "@vaadin/hilla-frontend";
 import client_1 from "./connect-client.default.js";
 async function chat_1(question: string, init?: EndpointRequestInit_1): Promise<string> { return client_1.call("ChatAiService", "chat", { question }, init); }
-export { chat_1 as chat };
+async function rag_1(question: string, init?: EndpointRequestInit_1): Promise<string> { return client_1.call("ChatAiService", "rag", { question }, init); }
+export { chat_1 as chat, rag_1 as rag };
--- src/main/frontend/views/chat.tsx
+++ src/main/frontend/views/chat.tsx
@@ -4,22 +4,34 @@ import {ChatAiService} from "Frontend/generated/endpoints";
 import Markdown from "react-markdown";
 
 export default function chat(){
-    const [question, setQuestion] = useState<String>("");
-    const [response, setResponse] = useState<String >("");
+    const [question, setQuestion] = useState<string>("");
+    const [response, setResponse] = useState<string >("");
 
     async function sendQuestion(){
         ChatAiService.chat(question)
             .then(response => setResponse(response))
             .catch();
     }
+    async function ragQuestion(){
+        ChatAiService.rag(question)
+            .then(response => setResponse(response))
+            .catch();
+    }
     return (
         <div className="p-m">
-            <h1>Chat</h1>
+            <h1>Simple ChatBot</h1>
             <div>
                 <TextField
                     className="w-full" label="Message"
                     onChange={e => setQuestion(e.target.value)}></TextField>
                 <Button theme="primary" onClick={sendQuestion}>Send</Button>
+            </div>
+            <h1>RAG ChatBot</h1>
+            <div>
+                <TextField
+                    className="w-full" label="Message"
+                    onChange={e => setQuestion(e.target.value)}></TextField>
+                <Button theme="primary" onClick={ragQuestion}>Send</Button>
                 <div>
                     <Markdown>
                         {response}
